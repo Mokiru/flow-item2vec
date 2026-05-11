1d81218b63eb96ca83aa4b04aaca0071548a5b08
ljk <lijingke@huice.com>
2026-05-11 09:56:21 +0800

    init

--- app/main.py
+++ app/main.py
@@ -1,19 +1,17 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
-from app.api.train_api import router as train_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    # 启动时：你的 datasource 里的预拉取线程会自动在 __init__ 里启动
-    print("Item2Vec Service Started.")
+    print("Service Started.")
     yield
     # 关闭时清理
-    print("Item2Vec Service Shutting down.")
+    print("Service Shutting down.")
 
 app = FastAPI(title="Item2Vec Service", lifespan=lifespan)
 
 # 注册路由
-app.include_router(train_router)
+# app.include_router(train_router)
 
 @app.get("/health")
 async def health():
